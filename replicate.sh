43627a6e874b75f614e29b05f06df443b1d13712
arve0 <arve.seljebu@gmail.com>
2015-02-18 14:36:08 +0100

    commands to replicate this repo

---
+++ replicate.sh
@@ -0,0 +1,35 @@
+# clone empty repo
+git clone https://github.com/arve0/example_lessons
+cd example_lessons/
+
+# clone builder
+git clone --depth 1 https://github.com/arve0/codeclub_lesson_builder
+
+# get UK lessons
+git clone --depth 1 https://github.com/CodeClub/python-curriculum
+git clone --depth 1 https://github.com/CodeClub/webdev-curriculum
+git clone --depth 1 https://github.com/CodeClub/scratch-curriculum
+
+# copy english lessons to src folder
+mkdir src
+cp -r python-curriculum/en-GB/ src/python
+cp -r scratch-curriculum/en-GB/ src/scratch
+cp -r webdev-curriculum/en-GB/ src/web
+
+# fix format of yaml headers and code blocks
+find src/ -name "*.md" -print0 | xargs -0 -L 1 codeclub_lesson_builder/utils/fix_yaml_header.sh 
+find src/ -name "*.md" -print0 | xargs -0 -L 1 codeclub_lesson_builder/utils/fix_code_blocks.sh 
+
+# fix YAML in one of the files manually
+sed -i '' 's/\.\.\./---/g' src/python/lessons/Minecraft2D/Minecraft2D.md
+
+# create index.md
+echo -e "---\ntitle: welcome\ntemplate: index.jade\n---\n# welcome\n... to this demo of [codeclub_lesson_builder](https://github.com/arve0/codeclub_lesson_builder)" > src/index.md
+
+# copy setup script and run it
+cp ./codeclub_lesson_builder/utils/setup .
+./setup
+
+# copy culp shortcuts and build
+cp codeclub_lesson_builder/utils/gulp* .
+./gulp
